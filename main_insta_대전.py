6c3e2c9a90e8557e2bb3857b7d6bbcf2d6ec2fc1
MSGDev <milk6645@naver.com>
2022-04-06 08:07:28 +0900

    id lst

--- main_insta_대전.py
+++ main_insta_대전.py
@@ -12,6 +12,11 @@ from urllib import parse
 import multiprocessing
 from bs4 import BeautifulSoup
 import datetime as datetime
+from selenium.webdriver.common.keys import Keys
+import random
+import pyautogui
+
+
 
 def searchKeyword(driver, strkeyword):
     url = "https://www.instagram.com/explore/tags/{}/".format(strkeyword)
@@ -23,9 +28,39 @@ def searchUserId(driver, strUserId):
     driver.get(url)
     time.sleep(3)
 
+def set_scrolling2():
+    element = driver.find_element_by_xpath("//body")
+
+    while True:
+       status = 0
+       last_height = driver.execute_script("return document.body.scrollHeight")
+
+
+       while True:
+           status +=1
+
+           if status == 14 or status == 27 or status == 40:
+               element.send_keys(Keys.PAGE_UP)
+
+           if status == 10 or status == 32 or status == 48:
+               element.send_keys(Keys.SPACE)
+
+           element.send_keys(Keys.PAGE_DOWN)
+           element.send_keys(Keys.SPACE)
+           time.sleep(random.uniform(1.2, 2.5))
+
+           if status == 40:
+               break
+
+       new_height = driver.execute_script("return document.body.scrollHeight")
+       print(new_height, last_height)
+       if new_height == last_height:
+           break
+       else:
+           last_height = new_height
+           continue
+
 def set_scrolling(driver):
-    from selenium.webdriver.common.keys import Keys
-    import random
 
     element = driver.find_element_by_xpath("//body")
 
@@ -45,7 +80,7 @@ def set_scrolling(driver):
 
            element.send_keys(Keys.PAGE_DOWN)
            # element.send_keys(Keys.SPACE)
-           time.sleep(random.uniform(5.5, 9.5))
+           time.sleep(random.uniform(1.5, 2.5))
 
            if status == 40:
                break
@@ -382,6 +417,37 @@ def detail_get(main_id_data):
              }
         )
 
+def computer_mode(driver, id_pw_cnt, id_lst, pw_lst):
+    elem_id = WD.web_driver_wait(driver, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+    elem_pw = WD.web_driver_wait(driver, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+    elem_id.clear()
+
+    elem_id.send_keys(id_lst[id_pw_cnt])
+
+    time.sleep(1)
+    elem_pw.clear()
+    elem_pw.send_keys(pw_lst[id_pw_cnt])
+    time.sleep(2)
+    WD.web_driver_wait(driver, '//*[@id="loginForm"]/div/div[3]/button/div').click()
+    time.sleep(5)
+
+def mobile_mode(driver, id_pw_cnt, id_lst, pw_lst):
+    # login button
+    time.sleep(10)
+    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div/div/div/div[3]/button[1]').click()
+
+    elem_id = WD.web_driver_wait(driver, '//*[@id="loginForm"]/div[1]/div[3]/div/label/input')
+    elem_pw = WD.web_driver_wait(driver, '//*[@id="loginForm"]/div[1]/div[4]/div/label/input')
+    elem_id.clear()
+
+    elem_id.send_keys(id_lst[id_pw_cnt])
+
+    time.sleep(1)
+    elem_pw.clear()
+    elem_pw.send_keys(pw_lst[id_pw_cnt])
+    time.sleep(2)
+    WD.web_driver_wait(driver, '//*[@id="loginForm"]/div[1]/div[6]/button/div').click()
+    time.sleep(5)
 
 if __name__ == '__main__':
 
@@ -391,27 +457,23 @@ if __name__ == '__main__':
     url_cnt = 0
     url_lst = []
 
-    url_lst.append("https://www.instagram.com/explore/tags/tagname")
 
-    driver = WD.browser_open('', 'https://www.instagram.com/', headress_mode=False, proxy_ip="", mobile_mode=False)
+    url_lst.append("https://www.instagram.com/explore/tags/태그명")
+
+
+
 
     # pickle.dump(driver.get_cookies(), open("cookies_mskpro1234.pkl", "wb"))
     # cookies = pickle.load(open("cookies_msgpro00001.pkl", "rb"))
     # for cookie in cookies:
     #     driver.add_cookie(cookie)
 
-    elem_id = WD.web_driver_wait(driver, '//*[@id="loginForm"]/div/div[1]/div/label/input')
-    elem_pw = WD.web_driver_wait(driver, '//*[@id="loginForm"]/div/div[2]/div/label/input')
-    elem_id.clear()
-    elem_id.send_keys("msgpro1234@gmail.com")
+    mobile = True
+    id_pw_cnt = 0
+    id_lst = []
+    pw_lst = []
 
-    time.sleep(1)
-    elem_pw.clear()
-    elem_pw.send_keys("pw")
-    time.sleep(2)
-    WD.web_driver_wait(driver, '//*[@id="loginForm"]/div/div[3]/button/div').click()
 
-    time.sleep(5)
 
     while True:
 
@@ -420,6 +482,14 @@ if __name__ == '__main__':
 
         lst = []
         url = url_lst[url_cnt]
+
+        driver = WD.browser_open('', 'https://www.instagram.com/', headress_mode=False, proxy_ip="", mobile_mode=mobile)
+        if mobile_mode:
+            mobile_mode(driver, id_pw_cnt, id_lst, pw_lst)
+        else:
+            computer_mode(driver, id_pw_cnt, id_lst, pw_lst)
+
+
         driver.get(url)
         set_scrolling(driver)
         get_network_header(driver, url)
@@ -427,6 +497,12 @@ if __name__ == '__main__':
 
         # driver.close()
         print(time.strftime("%y/%m/%d %H:%M:%S"))
-        del driver.requests
+
+        # del driver.requests
+        driver.close()
+
         data = 0
         url_cnt += 1
+        id_pw_cnt += 1
+        if id_pw_cnt == len(id_lst):
+            id_pw_cnt = 0
